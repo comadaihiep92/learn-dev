7c2bc8c48adde0ab33c4d5e010c27e7956eb9d76
Tran Hoang Lan <lan.tran@madison-technology.com>
2025-02-12 22:48:37 +0700

    new

---
+++ front-end/src/components/Recently.jsx
@@ -0,0 +1,89 @@
+import { Button } from "antd"
+import {ArrowRightOutlined } from "@ant-design/icons"
+import "../css/receiver.css"
+
+function Recently() {
+    return (
+        <div className="recently">
+            <div className="box-1">
+                <div className="title">
+                    <h3>Brackest</h3>
+                </div>
+                <div className="content">
+                    <p>Saving money - is something we would all like.</p>
+                </div>
+                <div className="avata">
+                    <img src="https://s3-alpha-sig.figma.com/img/3aad/0b59/0fbd8549255ac33539573708a31bf71a?Expires=1740355200&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=MtOQNxh~QiRUAIAcecW9kXBJLBDKe1ts3OhF7ik7rLA142Vn14Eh2Ip4asGprVEEXRy5hUl8Zxsokz5LpLjXNo8iweWydSFcRZZ5XrfAxO49Ht19qa5RIFwZx9PeiXhz~DBU48vznJGT3n1TdWvHzVjpUfCZq0JltGsYwI5IxpVPiwsR7gGPnjHrADVmHRmyZ~LhJ-alMC5M3ya-utFR-rzUdKynsiKJf7bnO6sNEK40twWplvMOdL~zqNxVh-RWohJbVwUVolHE-ZquZ8p0vQOkxFREJHG3m9uqd22tA4MwrXpfHF2-OTgPoMNuWnMZeW-tbw3GUGhYj71-Uxi6uA__" alt=""/>
+                    <img src="https://s3-alpha-sig.figma.com/img/acf3/9828/abd76e56ce1690d9fa8f4ada1200b40b?Expires=1740355200&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=F4OZsT~X08iHFt7YGHLKrgKAoYtSDk7wuJNd65W6v3VV4Yxgvq3VNLOkqS5LMe05-ODIY6~pSZfw6k8Jdcru3h0FMEUgxwpb8E-tRsdN94a8a7mzDU2Z22QMRqVTV~PX8tWJ4YFc9hOlAjpRSHQQkT~iTbCZhuQ1HaANCE3XzjOSSRKdoBE7fpMViadtZLbUf8WB0Wz1UlD4tXnsmmDjAueg~kGZozkkxfcxECv5msCP6Z0UNrA5ovgQVvky0EVtMd1vJvWKSHDiMZMoIlqUHOPyLtKKF~63kndihGfmNgwFjBT6utydD974Jmquhn3NhkF3X~N9xOcKzqi8w2OSHg__" alt=""/>
+                </div>
+                <div className="action">
+                    <Button icon={<ArrowRightOutlined />}></Button>
+                </div>
+            </div>
+            <div className="box-2">
+                <div className="title">
+                    <h3>Puctures in the Sky</h3>
+                </div>
+                <div className="content">
+                    <p>In the last five to six years the FTA satellite receiver has become an everyday household electronic device.</p>
+                </div>
+                <div className="avata">
+                    <img src="https://s3-alpha-sig.figma.com/img/5f9a/4d75/859f98b574f0cdb87a39218356119f87?Expires=1740355200&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=af-08mp-V64GeQQyh0QgCXP8QnAa5-IR-~5OXXwarb4kLIfZwCBmzM02AhmtNVyWDe0g8TVHA4a4TZY-zH-ybfg3O4V0xpJbVHbk7gV-hmQD-NxEkRntreTCwC~HYW2yygtkpSpWNJM6o9-s-qZLhX3JJyjnOtr-XNa6JTo2E5gnxZVsB7HER1GI13tHns4MKdsfm25i2QBP2D0k55m0c2BZp8KhprHlIg~0KDvqvCxOlQKwnlbXn3YMS9LsaUxwpUsc4DOsI4rwbqpr2vuqB73jjkcZbWkYcjeWxtU6vazoiTJGnQdX6Xg6~6ObGPHz0vLsuKx7NhbKPATxTs5HEw__" alt=""/>
+                    <img src="https://s3-alpha-sig.figma.com/img/ae2a/f7f5/cedf22333000aefa9f0a518d48f49dd2?Expires=1740355200&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=Eyj3Z0gAhqri0gfgGjZvUX3jDKdd6Sul6ZSeCgoFtZ0jbyHhX39JKJNcjxInk7z1ctwSVxmkXmzSEwWthKnHTqo5I2KESqgOIG0eiy7i-HgavSwkM-FwJcHFqHjIcqHYuVLJuvtF12H8UkBZssXSXZ60RXRs6WP517~krGZeZCwRWH99SSZQOLSZnDKS-AVibzZTpqgLSjI7XaZSyXZroZ~0~sY9e0JLaFHB5XZkC2-3FAga~VaqFZP-MmJLVFU5DmYV9i4hLycIOcySEjArKeh6wz0F8lN~ByCjlQsveZLvIcXBlWc4Q704luWVG~SrDS5P9a4ETEquWJS~tzx6VA__" alt=""/>
+                    <img src="https://s3-alpha-sig.figma.com/img/878b/4c98/e740db766301dd653ad3feddcd4f1fa7?Expires=1740355200&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=dtrib06Db95iab6rjbISwhaye7i8h0mbd9CoxaDcqZhDJl204QM3Iw4LEsJa3nJ8veYkKi1Km03MXlCJhY~dz67MgxhTIibFfpzpuXWv1kwP2taQvwQdtGQ9B6UTfawBMEbswW7dD8XQa3BHVm5FlVO6F10u4~KbFU089M0yyQAcnikM5hyuApy6tGZ9uvAvDIF6~ihLYT5oQdWnwsPr-wwukKP51RGOasv9JD3si9R6pkS0VddX-zIk~3v-vSHKLljVGYEcZhh2b9NL-XXGsAAtTkj3iYArwlQTY3ot8MsAv7qMj--OTbmMSkDazbWOSHhn9qzxGajYJke4wVk-gg__" alt=""/>
+                    <img src="" alt="+44"/>
+                </div>
+                <div className="action">
+                    <Button icon={<ArrowRightOutlined />}></Button>
+                </div>
+            </div>
+            <div className="box-3">
+                <div className="title">
+                    <h3>How To Look Up</h3>
+                </div>
+                <div className="content">
+                    <p>Color is so powerful that it can persuade, motivate.</p>
+                </div>
+                <div className="avata">
+                    <img src="https://s3-alpha-sig.figma.com/img/c698/aee1/80cb400df930c563d1e1fdae7daacd82?Expires=1740355200&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=GvFjT21AjUNsQqeLcDOBbOaC~YJgEX-6xwWsQGRURyaBup4HWAxwowodUYlTNaGH7mz3cihT~QBY5Y3FdWqRdPUH0VZiN19HQVUjF1rgNwmPdo2WwkkIbIJiaWibfkvHwpsloC0fKsQVSFqUzZAtchBLc1isvNApPMajE0eq8Dnhz6kdvI3ET9mmqkmjizJ4YEkblJLwIfHrIqgwWfEOg73uznt-rMhoQ1GftyaTe-uYdZPdpalATgJOy2hAY3PX5rKccxVmz9iCCG-e3oUiFncCputWWNbM92ofs5uVs45lAPgXuxmFj3QESM~jZqaw07~0dtVkR6hVBAp2qOocWw__" alt=""/>
+                </div>
+                <div className="action">
+                    <Button icon={<ArrowRightOutlined />}></Button>
+                </div>
+            </div>
+            <div className="box-4">
+                <div className="title">
+                    <h3>The Universe Through A Child S Eyes</h3>
+                </div>
+                <div className="content">
+                    <p>When I was just starting 6th grade I got my first job. Paperboy! Boy, was I excited.</p>
+                </div>
+                <div className="avata">
+                <img src="https://s3-alpha-sig.figma.com/img/5f9a/4d75/859f98b574f0cdb87a39218356119f87?Expires=1740355200&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=af-08mp-V64GeQQyh0QgCXP8QnAa5-IR-~5OXXwarb4kLIfZwCBmzM02AhmtNVyWDe0g8TVHA4a4TZY-zH-ybfg3O4V0xpJbVHbk7gV-hmQD-NxEkRntreTCwC~HYW2yygtkpSpWNJM6o9-s-qZLhX3JJyjnOtr-XNa6JTo2E5gnxZVsB7HER1GI13tHns4MKdsfm25i2QBP2D0k55m0c2BZp8KhprHlIg~0KDvqvCxOlQKwnlbXn3YMS9LsaUxwpUsc4DOsI4rwbqpr2vuqB73jjkcZbWkYcjeWxtU6vazoiTJGnQdX6Xg6~6ObGPHz0vLsuKx7NhbKPATxTs5HEw__" alt=""/>
+                    <img src="https://s3-alpha-sig.figma.com/img/ae2a/f7f5/cedf22333000aefa9f0a518d48f49dd2?Expires=1740355200&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=Eyj3Z0gAhqri0gfgGjZvUX3jDKdd6Sul6ZSeCgoFtZ0jbyHhX39JKJNcjxInk7z1ctwSVxmkXmzSEwWthKnHTqo5I2KESqgOIG0eiy7i-HgavSwkM-FwJcHFqHjIcqHYuVLJuvtF12H8UkBZssXSXZ60RXRs6WP517~krGZeZCwRWH99SSZQOLSZnDKS-AVibzZTpqgLSjI7XaZSyXZroZ~0~sY9e0JLaFHB5XZkC2-3FAga~VaqFZP-MmJLVFU5DmYV9i4hLycIOcySEjArKeh6wz0F8lN~ByCjlQsveZLvIcXBlWc4Q704luWVG~SrDS5P9a4ETEquWJS~tzx6VA__" alt=""/>
+                    <img src="https://s3-alpha-sig.figma.com/img/878b/4c98/e740db766301dd653ad3feddcd4f1fa7?Expires=1740355200&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=dtrib06Db95iab6rjbISwhaye7i8h0mbd9CoxaDcqZhDJl204QM3Iw4LEsJa3nJ8veYkKi1Km03MXlCJhY~dz67MgxhTIibFfpzpuXWv1kwP2taQvwQdtGQ9B6UTfawBMEbswW7dD8XQa3BHVm5FlVO6F10u4~KbFU089M0yyQAcnikM5hyuApy6tGZ9uvAvDIF6~ihLYT5oQdWnwsPr-wwukKP51RGOasv9JD3si9R6pkS0VddX-zIk~3v-vSHKLljVGYEcZhh2b9NL-XXGsAAtTkj3iYArwlQTY3ot8MsAv7qMj--OTbmMSkDazbWOSHhn9qzxGajYJke4wVk-gg__" alt=""/>
+                    <img src="" alt="+44"/>
+                </div>
+                <div className="action">
+                    <Button icon={<ArrowRightOutlined />}></Button>
+                </div>
+            </div>
+            <div className="box-5">
+                <div className="title">
+                    <h3>Astronomy Or Astrology</h3>
+                </div>
+                <div className="content">
+                    <p>Conversations can be a tricky business.</p>
+                </div>
+                <div className="avata">
+                    <img src="https://s3-alpha-sig.figma.com/img/3aad/0b59/0fbd8549255ac33539573708a31bf71a?Expires=1740355200&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=MtOQNxh~QiRUAIAcecW9kXBJLBDKe1ts3OhF7ik7rLA142Vn14Eh2Ip4asGprVEEXRy5hUl8Zxsokz5LpLjXNo8iweWydSFcRZZ5XrfAxO49Ht19qa5RIFwZx9PeiXhz~DBU48vznJGT3n1TdWvHzVjpUfCZq0JltGsYwI5IxpVPiwsR7gGPnjHrADVmHRmyZ~LhJ-alMC5M3ya-utFR-rzUdKynsiKJf7bnO6sNEK40twWplvMOdL~zqNxVh-RWohJbVwUVolHE-ZquZ8p0vQOkxFREJHG3m9uqd22tA4MwrXpfHF2-OTgPoMNuWnMZeW-tbw3GUGhYj71-Uxi6uA__" alt=""/>
+                    <img src="https://s3-alpha-sig.figma.com/img/878b/4c98/e740db766301dd653ad3feddcd4f1fa7?Expires=1740355200&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=dtrib06Db95iab6rjbISwhaye7i8h0mbd9CoxaDcqZhDJl204QM3Iw4LEsJa3nJ8veYkKi1Km03MXlCJhY~dz67MgxhTIibFfpzpuXWv1kwP2taQvwQdtGQ9B6UTfawBMEbswW7dD8XQa3BHVm5FlVO6F10u4~KbFU089M0yyQAcnikM5hyuApy6tGZ9uvAvDIF6~ihLYT5oQdWnwsPr-wwukKP51RGOasv9JD3si9R6pkS0VddX-zIk~3v-vSHKLljVGYEcZhh2b9NL-XXGsAAtTkj3iYArwlQTY3ot8MsAv7qMj--OTbmMSkDazbWOSHhn9qzxGajYJke4wVk-gg__" alt=""/>
+                </div>
+                <div className="action">
+                    <Button icon={<ArrowRightOutlined />}></Button>
+                </div>
+            </div>
+        </div>
+    )
+}
+export default Recently
